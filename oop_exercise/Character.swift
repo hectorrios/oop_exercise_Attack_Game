9ef6ab346743b02b30ba6f0ccebb5538c399c858
Hector Rios <oj.rios@gmail.com>
2016-03-09 23:18:27 +0100

    Character class and background and ground

---
+++ oop_exercise/Character.swift
@@ -0,0 +1,45 @@
+//
+//  Character.swift
+//  oop_exercise
+//
+//  Created by Hector Rios on 09/03/16.
+//  Copyright © 2016 mosby. All rights reserved.
+//
+
+import Foundation
+
+class Character {
+    
+    private var _hp : Int
+    private var _name : String
+    private var _attackPwr : Int
+
+    //MARK: Setter and Getters
+    var hp : Int {
+        get {
+            return _hp
+        }
+    }
+    
+    var name : String {
+        get {
+            return self._name
+        }
+    }
+    
+    var attackPwr: Int {
+        get {
+            return self._attackPwr
+        }
+    }
+    
+    //MARK: Initializers
+    init(name: String, startingHp: Int, attackPwr: Int) {
+        self._name = name
+        
+        //default hp and attackPwr
+        self._hp = startingHp
+        self._attackPwr = attackPwr
+    }
+    
+}
